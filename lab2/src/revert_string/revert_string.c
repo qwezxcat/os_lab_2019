c46eb7118661fb0500860a19e1eaaa0e8bbdfa6d
Umom <134609154+qwezxcat@users.noreply.github.com>
2023-09-28 21:50:04 +0000

    2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,12 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	char* tmp = malloc(sizeof(char)* strlen(str)+1);
+	int n = strlen(str);
+	for (int i = n-1; i>=0; i--){
+		tmp[n-i-1]=str[i];
+	}
+	strcpy(str,tmp);
+	free(tmp);
 }
 
